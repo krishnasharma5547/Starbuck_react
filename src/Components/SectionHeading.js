9c1f7d858cb4b39f338c3c3e783c8ac8184e5ef8
Krishna Kant Sharma <kksharmaz786@gmail.com>
2021-07-05 17:59:21 +0530

    Section Heading added

--- src/Components/SectionHeading.js
+++ src/Components/SectionHeading.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 export default function SectionHeading(props) {
+    let heading = "";
+    if (props.heading_classes === "" | undefined | null) {
+        heading = "text-3xl font-bold ";
+    } else {
+        heading = "";
+    }
     return (
-            <div className="text-center">
-                <h1 className="text-3xl font-bold">{props.title}</h1>
-                <p className="mt-4 text-lg mx-auto max-w-3xl ">
-                    {props.desc}
-                </p>
-            </div>
+
+        <div className="text-center mt-12 sm:mt-24">
+            <h1 className={heading + props.heading_classes}>{props.title}</h1>
+            <p className={"mt-4 text-lg mx-auto max-w-3xl " + props.child_classes}>
+                {props.children}
+            </p>
+        </div>
     )
 }
